27850d7230754f7e4c6d8f68a8a04e262bd0a974
Lucas Brentano <brentano.l@gmail.com>
2023-05-22 19:18:02 -0300

    basic widget log animation

--- main_menu/Actions.gd
+++ main_menu/Actions.gd
@@ -25,6 +25,7 @@ func _on_save_button_pressed():
 	
 	if not DirAccess.dir_exists_absolute(exitsave_path):
 		log_label.text = "Error: Exitsave don't exist."
+		widget.animation.play("error_message")		
 		return
 		
 	var character_save_path: String = save_path + "/" + current_character
@@ -34,20 +35,24 @@ func _on_save_button_pressed():
 	
 	if _copy_dir_files(exitsave_path, character_save_path) != OK:
 		log_label.text = "An error occured when copying save files"
+		widget.animation.play("error_message")		
 		return
 
 	log_label.text = "Exitsave copied with sucess"
+	widget.animation.play("ok_message")
 
 
 func _on_insert_button_pressed():
 	var backup_path : String = Config.get_value("settings", "save_directory", "user://") + "/" + save_folder_name + "/" + current_character
 	if not DirAccess.dir_exists_absolute(backup_path):
 		log_label.text = "Error: No save directory found with this character"
+		widget.animation.play("error_message")		
 		return
 	
 	var files = DirAccess.get_files_at(backup_path)
 	if len(files) < 3:
 		log_label.text = "Error reading backup save files"
+		widget.animation.play("error_message")
 		return
 	
 	var char_save_path : String = Config.get_value("settings", "stoneshard_directory") + "/characters_v1/" + current_character + "/exitsave_1" 
@@ -56,6 +61,7 @@ func _on_insert_button_pressed():
 		
 	if _copy_dir_files(backup_path, char_save_path) != OK: return
 	log_label.text = "Exitsave Inserted with sucess"
+	widget.animation.play("ok_message")
 
 
 func _copy_dir_files(from: String, to: String) -> Error:
--- save_widget/save_widget.gd
+++ save_widget/save_widget.gd
@@ -1,7 +1,12 @@
 extends Window
 
+@export var ok_icon: Texture2D
+@export var error_icon: Texture2D
+
 @onready var button_save := $Panel/HBoxContainer/SaveButton
 @onready var button_insert := $Panel/HBoxContainer/InsertButton
+@onready var log_texture := $Panel/HBoxContainer/MarginContainer/TextureRect
+@onready var animation := $AnimationPlayer
 
 var base_size : Vector2i
 
@@ -12,3 +17,9 @@ func _ready():
 func scale_size(scale: float) -> void:
 	size = base_size * scale
 	content_scale_factor = scale
+
+func _set_ok_icon() -> void:
+	log_texture.texture = ok_icon
+
+func _set_error_icon() -> void:
+	log_texture.texture = error_icon
