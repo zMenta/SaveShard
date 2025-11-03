401386d282214b69a90fe4931f72f62b1b5523e6
zMenta <brentano.l@gmail.com>
2025-11-03 16:16:41 -0300

    typo fix again

--- main_menu/Actions.gd
+++ main_menu/Actions.gd
@@ -84,7 +84,7 @@ func _on_insert_button_pressed(enable_widget_message: bool = true):
 		DirAccess.make_dir_absolute(char_save_path)
 		
 	if _copy_dir_files(backup_path, char_save_path) != OK: return
-	log_label.text = "%s: Exitsave Inserted successfully" % current_character
+	log_label.text = "%s: Exitsave inserted successfully" % current_character
 	if enable_widget_message: widget.animation.play("ok_message")
 	animation.play("sucess_log")
 
